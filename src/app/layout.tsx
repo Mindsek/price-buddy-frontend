8ed80db159b1e3a22da060334dc449689e4e658e
maxstein <supremetnf54@gmail.com>
2025-03-17 11:58:07 +0100

    fix config manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import Provider from "@/lib/provider";
 import type { Metadata } from "next";
 import "./globals.css";
-
 export const metadata: Metadata = {
   title: "Price Buddy",
   description: "Un comparateur de prix simple et efficace.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "pwa", "next-pwa", "price-comparator"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#007bff" }],
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/icons/ic_price_buddy_192.png" },
+    { rel: "icon", url: "/icons/ic_price_buddy_192.png" },
+  ],
 };
 
 export default function RootLayout({
@@ -14,9 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body suppressHydrationWarning>
         <Provider>
           <main className="flex-1">{children}</main>
