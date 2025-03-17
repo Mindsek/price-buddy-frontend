ed3fc06ca951255e8f6923d060df0772e81ca7cc
Timber <contactmindsek@gmail.com>
2025-03-17 12:31:46 +0100

    fix: fix build next

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import withPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
+
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
