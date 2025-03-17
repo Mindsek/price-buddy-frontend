6de8356d33a486feac1317b9b1556f9e84561d2f
Timber <contactmindsek@gmail.com>
2025-03-17 12:42:46 +0100

    fix: fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import { Provider as NextAuthProvider } from "@/lib/provider";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
+
 export const metadata: Metadata = {
-  title: "Price Buddy",
+  title: {
+    default: "Price Buddy",
+    template: "%s | Price Buddy",
+  },
   description: "Un comparateur de prix simple et efficace.",
   generator: "Next.js",
   manifest: "/manifest.json",
   keywords: ["nextjs", "pwa", "next-pwa", "price-comparator"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#007bff" }],
-  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
   icons: [
     { rel: "apple-touch-icon", url: "/icons/ic_price_buddy_192.png" },
     { rel: "icon", url: "/icons/ic_price_buddy_192.png" },
   ],
+};
+
+export const viewport: Viewport = {
+  themeColor: "#007bff",
 };
 
 export default function RootLayout({
