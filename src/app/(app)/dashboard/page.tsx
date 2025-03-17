21b44c49cf4dc3eb5736da3d3b0c62a8bfd2ca87
Timber <contactmindsek@gmail.com>
2025-03-17 15:43:15 +0100

    feat: add dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,3 +1,7 @@
+import { getDashboardData } from "@/app/actions/dashboard";
+import DashboardPage from "@/components/pages/dashboard/dashboard";
+
 export default async function Dashboard() {
-  return <div className="h-full">Dashboard</div>;
+  const data = await getDashboardData();
+  return <DashboardPage data={data} />;
 }
