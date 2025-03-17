5956580cae4942b4ae3039ff7acf2ea6bc1c7adc
Timber <contactmindsek@gmail.com>
2025-03-17 10:27:08 +0100

    feat: add auth with github
    add next-auth, add auth/prisma-adapter
    add provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex min-h-screen justify-center items-center flex-col">
+      Hello
+      <div>
+        {session?.user?.name ? (
+          <button onClick={() => signOut()}>Se déconnecter</button>
+        ) : (
+          <button onClick={() => signIn("github")}>Se connecter</button>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { prisma } from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      console.log("session", session);
+      return session;
+    },
+  },
+});
